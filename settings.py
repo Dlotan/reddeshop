ab92da4cdf5882a65af1ddb1f1d65288980ed709
Dlotan <lordkillelord@googlemail.com>
2014-07-26 10:22:20 -0700

    settings deleted

--- settings.py
+++
@@ -1,9 +0,0 @@
-#Admin Data
-right_admin_username = ""
-right_admin_password = ""
-admin_secret = "5e4r6t7zughkjftzTHZJHGFfghdsadgguzuhijonu98iokml"
-#Paypal
-#PP_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr" #sandbox
-#ACCOUNT_EMAIL= "florianseller@gmail.com" #sandbox
-PP_URL = "https://www.paypal.com/cgi-bin/webscr"
-ACCOUNT_EMAIL = "madcatsclothing@gmail.com"
